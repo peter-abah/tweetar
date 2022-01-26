cd674f47244135a86fc18c313513b66d96c9bae3
Peter Abah <peterabah.ob@gmail.com>
2022-01-26 23:48:10 +0100

    Add data module to mock data

---
+++ src/data/index.ts
@@ -0,0 +1,49 @@
+import faker from "@faker-js/faker";
+
+export interface Tweet {
+  body: string;
+  id: number;
+  user_id: number;
+  created_at: number;
+};
+
+const data = [];
+
+const generateUsers = (n = 25) => {
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    const user = {
+      ...faker.helpers.userCard(),
+      id: i,
+    };
+    result.push(user);
+  }
+
+  return result;
+};
+
+const generateTweets = (n = 25) => {
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    const tweet = {
+      body: faker.lorem.paragraph(),
+      id: i,
+      user_id: Math.floor(Math.random() * n),
+      created_at: new Date().getUTCMilliseconds,
+    };
+    result.push(tweet);
+  }
+
+  return result;
+};
+
+const users = generateUsers(50);
+const tweets = generateTweets(50);
+
+export const getTweets = () => {
+  return tweets;
+}
+
+export const getUsers = () => {
+  return users;
+}
