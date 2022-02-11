6ad9c4c9ff545219cc72b017390dd6213bbccb4b
Peter Abah <peterabah.ob@gmail.com>
2022-02-11 18:59:30 +0100

    Add auth module for sending authentication requests

---
+++ src/api/auth.ts
@@ -0,0 +1,42 @@
+import { baseUrl } from ".";
+
+export interface User {
+  name: string;
+  username: string;
+  email: string;
+  profile_image_url: string;
+  cover_image_url: string;
+  authentication_token: string;
+}
+
+interface Error {
+  error: string;
+}
+
+export type AuthResponse = User | Error;
+
+export const loginUser = async (username: string, password: string) => {
+  const requestBody = JSON.stringify({ username, password });
+  const response = await fetch(`${baseUrl}/login`, {
+    method: "POST",
+    mode: "cors",
+    body: requestBody,
+  });
+
+  return (await response.json()) as AuthResponse;
+};
+
+export const signUpUser = async (
+  username: string,
+  password: string,
+  passwordConfirmation: string
+) => {
+  const requestBody = JSON.stringify({ username, password });
+  const response = await fetch(`${baseUrl}/register`, {
+    method: "POST",
+    mode: "cors",
+    body: requestBody,
+  });
+
+  return (await response.json()) as AuthResponse;
+};
